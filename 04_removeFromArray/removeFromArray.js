05f9a318b8bb8705317c2b5b284c8187d38a128d
deadpool <118651379+DocStrange31@users.noreply.github.com>
2024-07-11 15:36:19 +0530

    completed ex 04 in js-exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
 const removeFromArray = function() {
+    let arr=arguments[0];
+    for(let i=1;i<arguments.length;i++){
+        while(arr.includes(arguments[i])) arr.splice(arr.indexOf(arguments[i]),1);
+    }
+    return arr;
 };
 
 // Do not edit below this line
